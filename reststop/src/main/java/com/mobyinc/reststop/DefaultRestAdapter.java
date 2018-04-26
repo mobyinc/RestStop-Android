3ebeb2b985e9e2ff6ef4fb5750904e599291361c
Corey Johnson <cor.johnson@me.com>
2018-04-25 18:30:46 -0700

    Work in progress on generics. Gson is not a fan of generics, has trouble deserializing to correct type unless you pass in type tokens.

--- reststop/src/main/java/com/mobyinc/reststop/DefaultRestAdapter.java
+++ reststop/src/main/java/com/mobyinc/reststop/DefaultRestAdapter.java
@@ -1,11 +1,21 @@
 package com.mobyinc.reststop;
 
 import android.content.Context;
+import android.util.Log;
 
+import com.android.volley.Request;
 import com.android.volley.RequestQueue;
+import com.android.volley.toolbox.StringRequest;
 import com.android.volley.toolbox.Volley;
+import com.google.gson.Gson;
+import com.google.gson.JsonArray;
+import com.google.gson.JsonElement;
 import com.google.gson.JsonObject;
+import com.google.gson.JsonParser;
 import com.mobyinc.reststop.toolbox.JsonObjectRequest;
+
+import java.lang.reflect.Type;
+import java.util.Collection;
 
 import io.reactivex.Single;
 
@@ -42,7 +52,35 @@ public class DefaultRestAdapter implements RestAdaptable {
     }
 
     @Override
-    public void getOne(String resourceName, String id) {
+    public <T> Single<T> getOne(String resourceName, String id, Type type) {
+        return Single.create(emitter -> {
+            StringRequest request = new StringRequest(
+                    Request.Method.GET,
+                    baseUrl + resourceName + "/" + id,
+                    response -> {
+                        JsonObject jsonObject = new JsonParser().parse(response).getAsJsonObject();
+                        emitter.onSuccess(new Gson().fromJson(jsonObject, type));
+                    },
+                    response -> emitter.onError(new Exception("Could not get object"))
+            );
+            queue.add(request);
+        });
+    }
 
+    @Override
+    public <T> Single<Collection<T>> getList(String resourceName, String id, Type type) {
+        return Single.create(emitter -> {
+            StringRequest request = new StringRequest(
+                    Request.Method.GET,
+                    baseUrl + resourceName + "/" + id,
+                    response -> {
+                        JsonElement jsonElement = new JsonParser().parse(response);
+
+                        emitter.onSuccess(new Gson().fromJson(jsonElement, type));
+                    },
+                    response -> emitter.onError(new Exception("Could not get object"))
+            );
+            queue.add(request);
+        });
     }
 }
--- reststop/src/main/java/com/mobyinc/reststop/RestAdaptable.java
+++ reststop/src/main/java/com/mobyinc/reststop/RestAdaptable.java
@@ -2,11 +2,15 @@ package com.mobyinc.reststop;
 
 import com.google.gson.JsonObject;
 
+import java.lang.reflect.Type;
+import java.util.Collection;
+
 import io.reactivex.Single;
 
 public interface RestAdaptable {
     //Create an auth response interface
     Single<JsonObject> authenticate(String username, String password, String clientId, String clientSecret);
     void setToken(String token);
-    void getOne(String resourceName, String id);
+    <T> Single<T> getOne(String resourceName, String id, Type type);
+    <T> Single<Collection<T>> getList(String resourceName, String id, Type type);
 }
